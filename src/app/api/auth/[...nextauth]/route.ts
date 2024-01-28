a3a90c9c911af73772453f31581870449d42110a
huuk000001 <huuk000001@gamil.com>
2024-01-28 19:18:34 +0900

    a

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,1 +1,5 @@
-export { GET, POST } from '@/auth';
+import { auth } from '@/auth'; // 단순히 파일 분리
+
+const handler = auth;
+
+export { handler as GET, handler as POST };
--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,4 @@
-import NextAuth from 'next-auth';
+import NextAuth, { AuthOptions } from 'next-auth';
 import NaverProvider from 'next-auth/providers/naver';
 import KakaoProvider from 'next-auth/providers/kakao';
 import GoogleProvider from 'next-auth/providers/google';
@@ -6,27 +6,7 @@ import AppleProvider from 'next-auth/providers/apple';
 import { createPrivateKey } from 'crypto';
 import { SignJWT } from 'jose';
 
-const getAppleToken = async () => {
-  const key = process.env.APPLE_PRIVATE_KEY;
-
-  const appleToken = await new SignJWT({})
-    .setAudience('https://appleid.apple.com')
-    .setIssuer(process.env.NEXT_PUBLIC_APPLE_TEAM_ID!)
-    .setIssuedAt(new Date().getTime() / 1000)
-    .setExpirationTime(new Date().getTime() / 1000 + 3600 * 2)
-    .setSubject(process.env.NEXT_PUBLIC_APPLE_ID!)
-    .setProtectedHeader({
-      alg: 'ES256',
-      kid: process.env.NEXT_PUBLIC_APPLE_ID,
-    })
-    .sign(createPrivateKey(key!));
-  return appleToken;
-};
-
-export const {
-  handlers: { GET, POST },
-  auth,
-} = NextAuth({
+export const authOption: AuthOptions = {
   cookies: {
     callbackUrl: {
       name: `__Secure-next-auth.callback-url`,
@@ -51,27 +31,24 @@ export const {
       clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
       clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
     }),
-    AppleProvider({
-      clientId: process.env.NEXT_PUBLIC_APPLE_ID,
-      clientSecret: await getAppleToken(),
-    }),
   ],
-  secret: process.env.NEXT_PUBLIC_SECRET,
+
   callbacks: {
-    async jwt(data) {
-      if (data.account) {
-        data.token.accessToken = data.account.access_token;
-        data.token.provider = data.account.provider;
-      }
-      return data.token;
+    async jwt({ token, user }: any) {
+      //user : provier return value
+      return token;
     },
-    async session({ session, token }) {
-      if (session) {
-        session.accessToken = token.accessToken;
-        session.provider = token.provider;
-        session.user.id = token.sub;
-      }
+
+    async session({ session, token }: any) {
+      //token : jwt return value
       return session;
     },
   },
-});
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+export const auth = async (req: any, res: any) => {
+  // authOption을 따로 만들지 않으면 getAppleToken 생성시 es2017 불필요. (async/await 사용)
+  // 굳이 authOption을 객체로 따로 둔 이유는 아래에 설명
+  return await NextAuth(req, res, authOption);
+};
