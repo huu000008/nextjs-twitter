6ef9df7544d701aa3d90017c2bfadf5ce094d32f
hyukrae <huuk000001@gamil.com>
2024-01-30 17:55:58 +0900

    ㅁ

--- src/app/(beforeLogin)/_component/Post.tsx
+++ src/app/(beforeLogin)/_component/Post.tsx
@@ -3,22 +3,21 @@
 import Image from 'next/image';
 import { getPosts } from '../_lib/getPosts';
 import { useQuery } from '@tanstack/react-query';
-import { Post as IPost } from '@/app/model/Post';
 
 export default function Post() {
-  const { data } = useQuery<IPost[]>({
+  const { data } = useQuery({
     queryKey: ['get', 'posts'],
     queryFn: getPosts,
   });
   console.log(data);
   return (
     <>
-      {data?.map((item: IPost) => {
-        <>
-          a
+      {data.map((item: any) => (
+        <div key={item.postId}>
+          <p>{item.content}</p>
           <Image src={item.imgUrl} alt="img" width={40} height={40} />;
-        </>;
-      })}
+        </div>
+      ))}
     </>
   );
 }
