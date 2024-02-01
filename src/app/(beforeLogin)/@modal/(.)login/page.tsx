7afce1ca3ff7b6ef42f66774d877683cf2bbf87e
hyukrae <huuk000001@gamil.com>
2024-02-01 16:52:52 +0900

    a

--- src/app/(beforeLogin)/@modal/(.)login/page.tsx
+++ src/app/(beforeLogin)/@modal/(.)login/page.tsx
@@ -7,6 +7,7 @@ import classNames from 'classnames/bind';
 import { useRouter } from 'next/navigation';
 import { signIn } from 'next-auth/react';
 import SnsLogin from '@/app/(beforeLogin)/_component/SnsLogin';
+import { motion } from 'framer-motion';
 
 export default function ModalLogin() {
   const cx = classNames.bind(styles);
@@ -37,31 +38,46 @@ export default function ModalLogin() {
     setPassword(e.target.value);
   };
 
+  const clickBg = () => {
+    router.back();
+  };
+
   return (
     <>
-      <div className={cx('wrap')}>
-        <form onSubmit={onSubmit}>
-          <input
-            value={id}
-            onChange={onChangeId}
-            type="text"
-            placeholder="id"
-            required
-          />
-          <input
-            value={password}
-            onChange={onChangePassword}
-            type="password"
-            placeholder="password"
-            required
-          />
-          <button type="submit" className={cx('button')}>
-            로그인
-          </button>
-        </form>
-        <div>or</div>
-        <SnsLogin />
-        <Link href={'/signup'}>회원가입</Link>
+      <div className={cx('wrap')} onClick={clickBg}>
+        <motion.div
+          className={cx('formWrap')}
+          initial={{ marginBottom: -20, opacity: 0 }}
+          animate={{ marginBottom: 0, opacity: 1 }}
+          exit={{ marginBottom: -20, opacity: 0 }}
+          transition={{ duration: 0.3 }}
+          onClick={e => e.stopPropagation()}
+        >
+          <form onSubmit={onSubmit}>
+            <input
+              value={id}
+              onChange={onChangeId}
+              type="text"
+              placeholder="id"
+              required
+            />
+            <input
+              value={password}
+              onChange={onChangePassword}
+              type="password"
+              placeholder="password"
+              required
+            />
+            <button type="submit" className={cx('button')}>
+              로그인
+            </button>
+          </form>
+          <div>or</div>
+          <SnsLogin />
+          <Link href={'/signup'} scroll={false}>
+            회원가입
+          </Link>
+        </motion.div>
       </div>
     </>
   );
